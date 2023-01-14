98d9a1668eae636411e815daca05981e7fba4541
graphsAB <122114750+graphsAB@users.noreply.github.com>
2023-01-14 15:21:17 +0300

    Add files via upload

---
+++ telegram-web-app.js
@@ -0,0 +1,1 @@
+attachMenuBot#c8aa2cd2 flags:# inactive:flags.0?true has_settings:flags.1?true bot_id:long short_name:string peer_types:Vector<AttachMenuPeerType> icons:Vector<AttachMenuBotIcon> = AttachMenuBot;
